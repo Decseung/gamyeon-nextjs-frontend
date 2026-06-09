491762ec9d3e2b7fa57e95f582941abc6e19e427
jiwon <joy.h7680@gmail.com>
2026-06-09 19:14:20 +0900

    refacotr: local npm run dev → clarity.init() 실행 안 함 배포 환경 → clarity.init() 기존처럼 실행

--- src/shared/lib/ClarityProvider.tsx
+++ src/shared/lib/ClarityProvider.tsx
@@ -8,7 +8,9 @@ interface ClarityProviderProps {
 }
 export default function ClarityProvider({ clarityId }: ClarityProviderProps) {
   useEffect(() => {
+    if (process.env.NODE_ENV === 'development') return
     if (!clarityId) return
+
     clarity.init(clarityId)
   }, [clarityId])
 
